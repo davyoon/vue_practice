749ed1b7bb73d012b7540d5603609d8f2d86dda8
David Yoon <david_yoon86@hotmail.com>
2019-02-07 14:25:58 -0500

    counter

--- first/script.js
+++ first/script.js
@@ -1,13 +1,17 @@
 new Vue({
 	el: '#app',
   data: {
-  	title: 'Hello World',
-  	link: 'http://google.com'
+  	counter: 0,
+  	x: 0,
+  	y: 0
   },
   methods: {
-  	sayHello: function(){
-  		this.title = 'Hello!';
-  		return this.title;
+  	increase: function(){
+  		this.counter ++;
+  	},
+  	updateCoordinates: function(event){
+  		this.x = event.clientX;
+  		this.y = event.clientY;
   	}
   }
 });
